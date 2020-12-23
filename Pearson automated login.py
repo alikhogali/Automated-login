ee0b204e51d9529b549c29cfc92f6afcff0ff0ef
shilkawi <71741556+shilkawi@users.noreply.github.com>
2020-12-22 20:50:55 -0500

    Add files via upload

---
+++ Pearson automated login.py
@@ -0,0 +1,17 @@
+from selenium import webdriver
+
+username = ("akhogali1")
+password = ("KLCnezL9")
+
+driver = webdriver.Chrome('C:\Program Files (x86)\chromedriver.exe')
+driver.get("https://login.pearson.com/v1/piapi/piui/signin?client_id=dN4bOBG0sGO9c9HADrifwQeqma5vjREy&okurl=https:%2F%2Fportal.mypearson.com%2Fcourse-home&siteid=8313")
+
+username_textbox = driver.find_element_by_name("username")
+username_textbox.send_keys(username)
+
+password_textbox = driver.find_element_by_name("password")
+password_textbox.send_keys(password)
+
+login_button = driver.find_element_by_id('mainButton')
+
+login_button.submit()
